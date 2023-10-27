3496803af2f5c40ce9f8a1b718a5a1a57ffbce09
Bramsouza <bruna.alvesmsouza@gmail.com>
2023-10-27 09:22:27 -0400

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,32 @@
+const accessCode1 = "VARIÁVEL";
+const accessCode2 = "FUNÇÃO";
+const accessCode3 = "BANCO DE DADOS";
+const accessCode4 = "COMPUTADOR";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V A I L V Á", 100,50)
+    fill("lightblue")
+    text("Dica: Sempre mudando, não constante!", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("Ç U N F O Ã", 700,150)
+    fill("lightblue")
+    text("Dica: Performa uma tarefa em particular!", 700,170)
+
+    fill("white")
+    textSize(15)
+    text("B N D A O C A S D O E D", 100,250)
+    fill("lightblue")
+    text("Dica: Armazena todas as informações!", 100,270)
+
+    fill("white")
+    textSize(15)
+    text("C M O P U A T R O D", 700,310)
+    fill("lightblue")
+    text("Dica: é um dispositivo!", 700,330)
+
+}
